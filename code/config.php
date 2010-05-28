8596fd15bdf9a93bf263ed0987f798c98f6d063a
unknown <jbrooks@.iq.iqsolutions.com>
2010-05-28 11:57:02 -0400

    more updates

--- code/config.php
+++ code/config.php
@@ -3,7 +3,7 @@
 define('SITENAME', 'ODS iPhone App 3');
 define('PATH_UNIX', 'C:\wamp\www\ODS\iphone3\/');
 define('PATH_WEB', 'http://192.168.1.10/ODS/iphone3/');
-define('THEME',  'apple'); // Select the iPhone base theme.
+define('THEME',  'jqt'); // Select the iPhone base theme.
 define('USERTHEME', 'default'); // User created addon theme.
 
 $db = array(
---
+++ content/0_page.php
@@ -0,0 +1,7 @@
+<p>Hey dudes, this is the main menu area.  Pretty cool, huh?  It looks about like an iPhone main menu should.</p>
+<p>The really cool part is switching themes by changing one variable initialization. No more changing a bunch of paths.</p>
+<ul class="rounded">
+  <li><a class="revealme" href="#page1">Test Reveal</a></li>
+  <li><a class="revealme" href="#page2">Test Reveal 2</a></li>
+  <li><a class="revealme" href="#page3">Test Reveal 3</a></li>
+</ul>
