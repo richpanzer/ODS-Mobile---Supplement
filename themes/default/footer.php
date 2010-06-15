c5334df0a6f591739a07a62c15bb3dc2321d18dd
unknown <jbrooks@.iq.iqsolutions.com>
2010-06-15 16:42:38 -0400

    a few more corrections

--- themes/default/footer.php
+++ themes/default/footer.php
@@ -1,31 +1,32 @@
+<?php
+$freeformOutput = <<<EOD
+var jQT = new $.jQTouch({
+  icon: '{$const['THEME_IMG']}jqtouch.png',
+  addGlossToIcon: false,
+  startupScreen: '{$const['THEME_IMG']}startup.png',
+  statusBar: 'black',
+  preloadImages: [
+      '{$const['JQTHEME']}img/back_button.png',
+      '{$const['JQTHEME']}img/back_button_clicked.png',
+      '{$const['JQTHEME']}img/button_clicked.png',
+      '{$const['JQTHEME']}img/grayButton.png',
+      '{$const['JQTHEME']}img/whiteButton.png',
+      '{$const['JQTHEME']}img/loading.gif'
+      ]
+});
+$(function(){
+  jQT.addAnimation({
+      name: 'reveal',
+      selector: '.revealme'
+  });
+});
+EOD;
+?>
   <script type="text/javascript">
     <?php
       include UNIX_JS . 'jquery-1.4.2.min.js';
       include UNIX_JQ . 'jqtouch/jqtouch.min.js';
-$freeformOutput = <<<EOD
-
-    var jQT = new $.jQTouch({
-      icon: '<?php echo THEME_IMG; ?>jqtouch.png',
-      addGlossToIcon: false,
-      startupScreen: '<?php echo THEME_IMG; ?>startup.png',
-      statusBar: 'black',
-      preloadImages: [
-          '<?php echo JQTHEME; ?>img/back_button.png',
-          '<?php echo JQTHEME; ?>img/back_button_clicked.png',
-          '<?php echo JQTHEME; ?>img/button_clicked.png',
-          '<?php echo JQTHEME; ?>img/grayButton.png',
-          '<?php echo JQTHEME; ?>img/whiteButton.png',
-          '<?php echo JQTHEME; ?>img/loading.gif'
-          ]
-    });
-    $(function(){
-      jQT.addAnimation({
-          name: 'reveal',
-          selector: '.revealme'
-      });
-    });
-
-EOD;
+      $const = get_defined_constants();
       echo addjstext($freeformOutput);
       // format default::: addjstofile($file, $minify=true);
       echo addjsfile(UNIX_JS . 'config.js');
