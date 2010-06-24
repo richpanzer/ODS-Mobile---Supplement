f23f6ee700fb77b833ce8e7eef466e5abe3a41ed
James Brooks <info@epiphanydigital.com>
2010-06-24 14:11:53 -0400

    minor update

--- index.php
+++ index.php
@@ -3,7 +3,10 @@
   include 'code/config.php';
 
   /*
-   * Not a whole lot of logic written into this file, eh? Update
+   * Not a whole lot of logic written into this file, eh?
+   * All the same, we need somewhere to start.  Probably wouldn't be a bad idea
+   * to put the inclusion statments for global.php and the template files
+   * here as well.
    */
 
 ?>
