778f24d058fbb7ae6772540a5bb24986bd16d62b
James Brooks <info@epiphanydigital.com>
2010-06-28 10:37:57 -0400

    bad link to "edit profile".  went to "add profile" instead.

--- content/Profile.php
+++ content/Profile.php
@@ -2,7 +2,7 @@
   <div class="toolbar">
     <h1 class="currentUser">Error</h1>
     <a class="button back" href="#Profiles">Profiles</a>
-    <a class="button pop" href="#Add_Profile">Edit</a>
+    <a class="button pop" href="#Update_Profile">Edit</a>
   </div>
   <ul id="profile_entries" class="rounded">
   </ul>
