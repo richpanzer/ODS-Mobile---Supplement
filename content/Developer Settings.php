6404b15d5a6eb0ee6a8d2df62d44c58863e804de
James Brooks <info@epiphanydigital.com>
2010-06-29 16:24:53 -0400

    some content cleanup. added a user settings page. moved developer settings page to a single text link at the bottom of the start screen.

---
+++ content/Developer Settings.php
@@ -0,0 +1,14 @@
+<div class="inner-has-bottomtools">
+  <div class="toolbar">
+      <h1>Dev.Settings</h1>
+      <a class="button goback" href="#Start">Done</a>
+  </div>
+  <div class="info">
+    <p>Need to start fresh?</p>
+  </div>
+  <div class="content">
+    <p><a href="#Start" id="resetUserData" class="pop whiteButton">Reset User Data</a></p>
+    <p><a href="#Start" id="populateUserData" class="pop whiteButton">Prepopulate Database</a></p>
+  </div>
+</div>
+<?php include PATH_TXT . 'includes/toolbar.php'; ?>
