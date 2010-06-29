93c51d135365e608154e4df219c68f71e1d21602
James Brooks <info@epiphanydigital.com>
2010-06-29 09:20:55 -0400

    moved developer settings page out of the start menu navigation. put in a better "update profile" placeholder

--- content/Update Profile.php
+++ content/Update Profile.php
@@ -3,8 +3,13 @@
       <h1>Update Profile</h1>
       <a class="blueButton goback pop" href="#Start">Done</a>
   </div>
-  <div class="content">
-    <p>Form to edit currently selected profile will appear here.</p>
+  <div class="info">
+    <p>This page is a placeholder.</p>
   </div>
+  <form>
+  <ul class="rounded">
+    <li><input type="text" placeholder="Rich Panzer" name="updateUser" value="" /></li>
+  </ul>
+  </form>
 </div>
 <?php include PATH_TXT . 'includes/toolbar.php'; ?>
