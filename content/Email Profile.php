f0c9155583a4f2d7625fa619ce67dd8a4ac7cc6f
James Brooks <info@epiphanydigital.com>
2010-06-30 11:37:21 -0400

    removed some comments

--- content/Email Profile.php
+++ content/Email Profile.php
@@ -3,24 +3,6 @@
       <h1>Email</h1>
       <a class="pop blueButton" href="#Profiles">Profiles</a>
   </div>
-  <?php /*
-  <span id="emailContent">
-    <ul class="rounded emailUserEntry">
-      <li><strong>James Brooks</strong></li>
-      <li>Supplement - Amount Frequency</li>
-      <li>Calcium - 8mg 3x Day</li>
-    </ul>
-    <ul class="rounded emailUserEntry">
-      <li><strong>Rich Panzer</strong></li>
-      <li>Supplement - Amount Frequency</li>
-      <li>Calcium - 8mg 3x Day</li>
-    </ul>
-    <ul class="rounded emailUserEntry">
-      <li><strong>Paulina Puig</strong></li>
-      <li>Supplement - Amount Frequency</li>
-      <li>Calcium - 8mg 3x Day</li>
-    </ul>
-  </span> */ ?>
   <div id="emailProfiles">
   </div>
   <div class="content">
