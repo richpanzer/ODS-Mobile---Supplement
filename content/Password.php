5462ced9d556f1bc8c23108a7d9cbf6ed3fba368
Rich Panzer <rpanzer@iqsolutions.com>
2010-07-01 11:09:40 -0400

    add password.php

---
+++ content/Password.php
@@ -0,0 +1,15 @@
+<div class="inner-has-bottomtools">
+  <div class="toolbar">
+      <h1>Password Protect App</h1>
+      <a class="blueButton goback pop" href="#Start">Done</a>
+  </div>
+  <div class="info">
+    <p>This page is a placeholder.</p>
+  </div>
+  <form>
+  <ul class="rounded">
+    <li><input type="text" placeholder="Rich Panzer" name="updateUser" value="" /></li>
+  </ul>
+  </form>
+</div>
+<?php include PATH_TXT . 'includes/toolbar.php'; ?>
