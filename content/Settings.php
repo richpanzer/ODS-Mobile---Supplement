2efcb03e21ac36c87b8511bb03942a181fe5b7c4
Rich Panzer <rpanzer@iqsolutions.com>
2010-07-01 10:51:11 -0400

    latest additions

--- content/Settings.php
+++ content/Settings.php
@@ -1,9 +1,17 @@
 <div class="inner-has-bottomtools">
   <div class="toolbar">
-      <h1>Settings</h1>
+      <h1>Additional Functionality</h1>
       <a class="button goback" href="#Start">Done</a>
   </div>
-  <div class="content">
+
+<ul class="edgetoedge">
+    <li><a href="#Instructions">Intstructions</a></li>
+    <li><a href="#Tips">Tips</a></li>
+    <li><a href="#Password">Password Protect App</a></li>
+  </ul>
+</div>
+
+ <!-- <div class="content">
     <p>Control your system settings here.</p>
   </div>
   <form id="mainSettings" method="post">
@@ -14,5 +22,6 @@
       <li><input type="submit" class="submit" name="action" value="Save" /></li>
     </ul>
   </form>
-</div>
+</div>-->
+
 <?php include PATH_TXT . 'includes/toolbar.php'; ?>
