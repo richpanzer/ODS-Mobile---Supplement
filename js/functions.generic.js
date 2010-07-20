0fa8af84ae32cb98d95399919d6e5d59cd3e67c1
James Brooks <info@epiphanydigital.com>
2010-07-20 14:36:38 -0400

    asdf

--- js/functions.generic.js
+++ js/functions.generic.js
@@ -167,8 +167,9 @@ function goToAccordion(clicked) {
 }
 
 function setPageHeight(clicked) {
-  //var pageheight = window.innerHeight != null? window.innerHeight : document.documentElement && document.documentElement.clientHeight ?  document.documentElement.clientHeight : document.body != null? document.body.clientHeight : null;
-  pageheight = $(window).height() - 140;
+  var pageheight = window.innerHeight != null? window.innerHeight : document.documentElement && document.documentElement.clientHeight ?  document.documentElement.clientHeight : document.body != null? document.body.clientHeight : null;
+  //pageheight = $(window).height() - 140;
+  pageheight = pageheight - 140;
   var homeheight = pageheight + 20;
   $('.hastoolbar_home').css('min-height',height + 'px');
   $('.hastoolbar').css('min-height',pageheight + 'px');
