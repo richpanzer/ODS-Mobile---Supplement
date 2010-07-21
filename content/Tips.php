481c294b9f6fd6cb3ca150fe1d005abfe4677ea9
James Brooks <info@epiphanydigital.com>
2010-07-21 11:12:30 -0400

    incorporated rich's content updates

--- content/Tips.php
+++ content/Tips.php
@@ -4,22 +4,20 @@
     <a class="button back" href="#Start">Home</a>
   </div>
   <ul class="edgetoedge">
-<<<<<<< HEAD
     <li><a class="instructProfileRef" href="#Instructions">Create multiple accounts.</a></li>
     <li><a class="instructPasswordRef" href="#Instructions">Password protect this app.</a></li>
     <li><a class="instructEmailRef" href="#Instructions">Email your profiles.</a></li>
     <li><a class="instructPhotoRef" href="#Instructions">Add up to two photos of each dietary supplement.</a></li>
     <li><a href="#Dietary_Supplement_Info">The latest ODS fact sheets on dietary supplements are available.</a></li>
     <li><a href="#Learn_About_ODS">Visit the Office of Dietary Supplements.</a></li>
-=======
+<?php /*
     <li><a class="instructPasswordRef" href="#">Password protect the application</a></li>
     <li><a class="instructAddRef" href="#">Add photos of each dietary supplement</a></li>
     <li><a class="instructEmailRef" href="#">Email profiles</a></li>
     <li><a class="instructProfileRef" href="#">Create multiple profiles</a></li>
     <li><a target="_blank" href="http://ods.od.nih.gov/">NIH Office of Dietary Supplements Web site</a></li> 
     <li><a href="#Dietary_Supplement_Info">Read fact sheets on dietary supplements</a></li>
-    	
->>>>>>> cbeeeafa3c9a6c9966747b4a2aef6485c28ecebe
+*/ ?>
   </ul>
 </div>
 <?php
