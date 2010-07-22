35d573ff5db62dda51b707ff848a8d7ebc9fa54a
James Brooks <info@epiphanydigital.com>
2010-07-22 14:25:53 -0400

    linking up the home page

--- content/includes/homeicons.php
+++ content/includes/homeicons.php
@@ -1,8 +1,8 @@
 <div class="toolbar_bottom">
   <div class="content">
     <ul class="footerLogos">
-      <li><a href="#home"><img src="<?php echo PATH_CONTENT; ?>img/logo1.png" alt="Some Text" /></a></li>
-      <li><a href="#home"><img src="<?php echo PATH_CONTENT; ?>img/logo2.png" alt="Some Text" /></a></li>
+      <li><a href="http://www.hhs.gov/" target="_blank"><img src="<?php echo PATH_CONTENT; ?>img/logo1.png" alt="Some Text" /></a></li>
+      <li><a href="http://ods.od.nih.gov/" target="_blank"><img src="<?php echo PATH_CONTENT; ?>img/logo2.png" alt="Some Text" /></a></li>
     </ul>
   </div>
 </div>
