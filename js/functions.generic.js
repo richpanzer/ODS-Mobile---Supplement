70228effdae272f575282e8242922481432bc7e8
James Brooks <info@epiphanydigital.com>
2010-07-29 15:07:45 -0400

    jklhygf

--- js/functions.generic.js
+++ js/functions.generic.js
@@ -183,7 +183,7 @@ function setPageHeight() {
     ph = 1004;
     tbh = 850;
   } else if (IsiPhone || IsiPod) {
-    tbh = 306;
+    tbh = 336;
   } else {
     ph = window.innerHeight != null? window.innerHeight : document.documentElement && document.documentElement.clientHeight ? document.documentElement.clientHeight : document.body != null? document.body.clientHeight : null;
     tbh = 306;
