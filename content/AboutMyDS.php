f29fc7d72e579947e00c6d12121bb66c2211533a
Rich Panzer <rpanzer@iqsolutions.com>
2010-07-29 09:50:29 -0400

    new files

---
+++ content/AboutMyDS.php
@@ -0,0 +1,11 @@
+<div class="toolbar">
+  <h1>About MyDS</h1>
+  <a class="button back" href="#Start">Home</a>
+</div>
+<div class="hastoolbar">
+  <p>Info about the App that people should already know before downloading the app.</p>
+</div>
+<?php
+$currentPage = '';
+include PATH_TXT . 'includes/toolbar.php';
+?>
---
+++ content/more.php
@@ -0,0 +1,17 @@
+<div class="toolbar">
+  <h3>More Functionality</h3>
+  <a class="button goback" href="#Start">Done</a>
+</div>
+<div class="hastoolbar">
+  <ul class="edgetoedge">
+    <li class="arrow"><a href="#Instructions">Instructions</a></li>
+    <li class="arrow"><a href="#AboutMyDS">About MyDS</a></li>
+    <li class="arrow"><a href="#Dietary_Supplement_Info">Resources</a></li>
+    <li class="arrow"><a href="#Email_Profile">Email Profile</a></li>
+    <li class="arrow"><a href="#Settings">Settings</a></li>
+  </ul>
+</div>
+<?php
+$currentPage = 'settings';
+include PATH_TXT . 'includes/toolbar.php';
+?>
