61dbed86e9bbeba33f8ced53b5f087c531e2b0b5
Rich Panzer <rpanzer@iqsolutions.com>
2010-07-29 13:19:18 -0400

    content updates

--- content/AboutMyDS.php
+++ content/AboutMyDS.php
@@ -3,7 +3,18 @@
   <a class="button back" href="#Start">Home</a>
 </div>
 <div class="hastoolbar">
-  <p>Info about the App that people should already know before downloading the app.</p>
+  <p>The Office of Dietary Supplements (ODS) at the National Institutes of Health (NIH) has developed MyDS  to help you provide complete and accurate information to your health care providers about which dietary supplements you take. You will have all of your data at your fingertips when you visit your doctor, other providers, or speak with them on the phone. With MyDS, you also can email your personal list of dietary supplements to yourself or to your health care providers as well as print it out.</p>
+<p><strong>Features:</strong>
+<ul>
+<li>Create personal dietary supplement profiles for yourself and others.</li>
+<li>Record the name and amount of each dietary supplement you take.</li>
+<li>Add additional information about each dietary supplement in the Notes field.</li>
+<li>Email your dietary supplement profile to yourself or your health care providers.</li>
+<li>Add up to two photos of each dietary supplement on your list.</li>
+<li>Protect your information with the option to create a personal password.</li>
+<li>Access reliable information about dietary supplements from the Office of Dietary Supplements</li>
+</ul>
+</p>
 </div>
 <?php
 $currentPage = '';
